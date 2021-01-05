ab4c398579cb51b7464454963ef470ee62e4a11a
Alexey <exelay.rapik@gmail.com>
2021-01-05 18:42:18 +0300

    Wrote simple drom scraper

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+type CarAd struct {
+	Title string
+	Price string
+}
+
+func main() {
+	results := []CarAd{}
+	c := colly.NewCollector()
+
+	c.OnHTML("a[data-ftid=\"bulls-list_bull\"]", func(e *colly.HTMLElement) {
+		temp := CarAd{}
+		temp.Title = e.ChildText("span[data-ftid=\"bull_title\"]")
+		temp.Price = e.ChildText("span[data-ftid=\"bull_price\"]")
+		results = append(results, temp)
+	})
+
+	c.Visit("https://spb.drom.ru/auto/")
+	fmt.Println(results)
+}
